fd6eced6716425bce8e1c52beb12d086949194f1
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-07-03 16:05:29 +0200

    Task to install IRkernel.

---
+++ roles/jupyterhub/files/irkernel_install.r
@@ -0,0 +1,6 @@
+#!/usr/bin/Rscript
+
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'), repos = c(CRAN = "http://cran-mirror.cs.uu.nl"))
+devtools::install_github('IRkernel/IRkernel')
+
+IRkernel::installspec(user = FALSE)
