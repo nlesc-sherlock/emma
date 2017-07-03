568d563609d751b6d65662b01c40436d4bc827f5
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-07-03 16:06:32 +0200

    Place it in the right place.

--- roles/jupyterhub/files/irkernel_install.r
+++ roles/jupyterhub/files/irkernel_install.r
@@ -1,5 +1,6 @@
 #!/usr/bin/Rscript
 
+# Instructions coming from https://irkernel.github.io/installation/
 install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'), repos = c(CRAN = "http://cran-mirror.cs.uu.nl"))
 devtools::install_github('IRkernel/IRkernel')
 
